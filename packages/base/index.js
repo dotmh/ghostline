37c8bcc4120b188a14b5630b2ba5e71458c13cbd
Martin Haynes <martin@dotmh.io>
2020-05-22 15:41:29 +0100

    start adding bundle base

---
+++ packages/base/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'handlebarTools' : require('@dotmh/tamahagane-handlebars-tools'),
+    'handlebarContents' : require('@dotmh/tamahagane-handlebars-content'),
+    'galleries' : require('@dotmh/tamahagane-galleries'),
+    'draftExcluder' : require('@dotmh/tamahagane-draft-exculder'),
+    'metaLoader' : require('')
+}
