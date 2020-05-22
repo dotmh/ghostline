18bb1c38642c9b10a1740299657a6903ed782252
Martin Haynes <martin@dotmh.io>
2020-05-22 15:48:09 +0100

    adds base bundle

--- packages/base/index.js
+++ packages/base/index.js
@@ -3,5 +3,10 @@ module.exports = {
     'handlebarContents' : require('@dotmh/tamahagane-handlebars-content'),
     'galleries' : require('@dotmh/tamahagane-galleries'),
     'draftExcluder' : require('@dotmh/tamahagane-draft-exculder'),
-    'metaLoader' : require('')
+    'metaLoader' : require('@dotmh/tamahagane-meta-loader'),
+    'includes' : require('@dotmh/tamahagane-includes'),
+    'inherit' : require('@dotmh/tamahagane-inherit'),
+    'isCurrent' : require('@dotmh/tamahagane-is-current'),
+    'list' : require('@dotmh/tamahagane-list'),
+    'imageManager' : require('@dotmh/tamahagane-image-manager')
 }
